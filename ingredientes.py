fd5207b13398f793bd783f4e71690711c04d0b6c
Valeria Fariña Rebolledo <146679101+ValeriaPFR@users.noreply.github.com>
2024-04-05 20:23:13 -0300

    Add ingredientes.py file. Includes ingredient options for pizza preparation

---
+++ ingredientes.py
@@ -0,0 +1,4 @@
+# lista para ingredientes para la preparacion de pizza
+proteina = ['pollo' , 'vacuno' , 'carne']
+vegetal = ['tomate' , 'aceituna' , 'champiñon']
+masa = ['tradicional' , 'delgada']

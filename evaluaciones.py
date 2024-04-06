e90a7ae8d92c40657184c5aeb86d8fa03325feb0
Valeria Fariña Rebolledo <146679101+ValeriaPFR@users.noreply.github.com>
2024-04-05 22:14:36 -0300

    Modify evaluaciones.py file. Interact with Pizza class for order handling and validation

--- evaluaciones.py
+++ evaluaciones.py
@@ -1,24 +1,25 @@
 # Importar la clase Pizza desde el modulo pizza.py
 from pizza import Pizza
+import ingredientes 
 
 """ 
 a. Valores de atributos de clase
 """
 # Obtener los valores de atributos de clase
-tamaño, precio = Pizza.tamaño, Pizza.precio
-print(f"El tamaño de la pizza es: {tamaño} y su precio es: ${precio}")
+tamanio, precio = Pizza.tamanio, Pizza.precio
+print(f"El tamaño de la pizza es: {tamanio} y su precio es: ${precio}")
 
 """ 
 b. Uso del método Pizza.validar()
 """
 # Validar un ingrediente usando el metodo Pizza.validar()
 texto = 'Salsa de tomate'
-lista = ['Salsa de tomate', 'Salsa Bbq']
+lista = ['Salsa de tomate', 'Salsa BBQ']
 test = Pizza.validar(texto, lista)
 if test:
-    print(f"El ingrediente, {texto}, está dentro de la lista de ingredientes")
+    print(f"El ingrediente, {texto}, está dentro de las opciones de ingredientes")
 else:
-    print("Ingrediente no válido")
+    print("Lo sentimos, ingrediente no disponible")
 
 """ 
 c. Solicitar ingredientes y tipo de masa al usuario
@@ -33,15 +34,19 @@ d. Imprimir la selección del usuario
 """
 print(f'''
 Su pedido es el siguiente:
-Proteína: {pedido.ingrediente_proteico}
-Vegetales: {pedido.ingrediente_vegetal1}, {pedido.ingrediente_vegetal2}
-Tipo de masa: {pedido.tipo_masa}
+Proteína:
+{ingredientes.proteicos[pedido.ingrediente_proteico-1]}
+Vegetales: 
+{ingredientes.vegetales[pedido.ingrediente_vegetal1-1]},
+{ingredientes.vegetales[pedido.ingrediente_vegetal2-1]}
+Tipo de masa: 
+{ingredientes.masas[pedido.tipo_masa-1]}
 ''')
 
 """ 
 e. Verificar si el pedido de pizza es valido
 """
 if pedido.es_valida:
-    print("Su pedido es válido")
+    print("Su pedido es válido. QUE DISFRUTE SU PIZZA :)")
 else:
-    print("Su pedido no es válido")
+    print("Su pedido no es válido :( \n. Intente con otra combinación de ingredientes")

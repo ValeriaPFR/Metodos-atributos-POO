1baacc4301a509118304807d243be5d2d12141b2
Valeria Fariña Rebolledo <146679101+ValeriaPFR@users.noreply.github.com>
2024-04-05 22:28:40 -0300

    Modify ingredientes.py file. Adds lists of ingredients with the options chosen by users and the correspondence with the selected ingredient for the final message printing.

--- ingredientes.py
+++ ingredientes.py
@@ -1,4 +1,15 @@
+"""
+Nota: 
+La lista 'proteina' corresponde al orden de aparición y elección de ingrediente. Es la opción que ingresa el usuario
+La lista 'proteicos'  contiene los nombres de proteínas, para identificar las opciones en el print final
+"""
 # lista para ingredientes para la preparacion de pizza
-proteina = ['pollo' , 'vacuno' , 'carne']
-vegetal = ['tomate' , 'aceituna' , 'champiñon']
-masa = ['tradicional' , 'delgada']
+proteina = [1,2,3]
+proteicos = ["Pollo", "Vacuno", "Proteína vegetal"]
+
+vegetal = [1,2,3]
+vegetales =["Tomate", "Aceituna", "Champiñón"]
+
+masa = [1,2]
+masas= ["Tradicional", "Delgada/A la Piedra"]
+
